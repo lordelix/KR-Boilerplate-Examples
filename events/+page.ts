06cfd6a2286e77ebca1196bdf6bd014db77bb680
Felix Heidecke <felix@heidecke.me>
2026-02-10 18:53:17 +0100

    5.4.0-17

--- events/+page.ts
+++ events/+page.ts
@@ -2,7 +2,7 @@ import { getEvents } from '$lib/boilerplate/xioni/cms/Events'
 import xioniLoader from '$lib/boilerplate/xioni/utils/xioniLoader'
 
 export async function load() {
-	const { events } = await xioniLoader(getEvents(1289, { endsAfter: new Date() }))
+	const { events } = await xioniLoader(getEvents(1289))
 
 	return { events }
 }
--- events/[event]_[id]/+page.svelte
+++ events/[event]_[id]/+page.svelte
@@ -1,31 +1,51 @@
-<script>
-	const { data } = $props()
+<script lang="ts">
+	import {
+		Button,
+		Checkbox,
+		Form,
+		Input,
+		Link,
+		Select,
+		Textarea,
+		Wrapper,
+		XioniEvent
+	} from '$lib/boilerplate/components/index.js'
 
-	let registration = $state(null)
+	const { data } = $props()
+	const event = $derived(data.event)
 </script>
 
-<Wrapper>
-	<XioniEvent class="$my-2" event={data.event} on:registrationButtonClick={registration.open} />
-</Wrapper>
+<Wrapper class="$py-4">
+	<XioniEvent class="$my-2" {event} />
 
-<XioniEventRegistration bind:this={registration} formId={4} event={data.event}>
-	<Input name="Name" label="Name" placeholder="Vor- und Nachname" class="$mt" required />
-	<Textarea name="Anschrift" label="Anschrift" class="$mt" required />
-	<Input name="Telefon" label="Telefonnummer" class="$mt" required />
-	<Input type="Email" name="Email" label="E-Mail-Adresse" class="$mt" required />
-	<Textarea rows="2" name="Nachricht" label="Nachricht" class="$mt" />
-	<Checkbox
-		name="datenschutz"
-		label="Ich habe die <a href='/datenschutz'>Datenschutzerklärung</a> zur Kenntnis genommen und stimme zu, dass meine Angaben und Daten zur Beantwortung meiner Anfrage elektronisch erhoben und gespeichert werden."
-		value="akzeptiert"
-		class="$mt"
-		required />
-	<div slot="done">
-		<hr />
-		<p>Vielen Dank für Ihre Anmeldung.</p>
+	{#if event.flags?.includes('anmeldung')}
+		<h3>Jetzt anmelden</h3>
+		<Form moduleId="4">
+			<input type="hidden" name="event-id" value={event.id} />
+			<input type="hidden" name="event-title" value={event.title} />
+
+			<div class="$flex $flex-column $gap">
+				<Input name="Teilnehmer" label="Teilnehmer" placeholder="Vor- und Nachname" required />
+				<Textarea name="Anschrift" label="Anschrift" required />
+				<Input name="Telefon" label="Telefonnummer" />
+				<Input name="Email" type="email" label="E-Mail-Adresse" required />
+				<Textarea name="Nachricht" label="Nachricht" rows="2" />
+				<Checkbox
+					name="datenschutz"
+					label="Ich habe die <a href='/datenschutz'>Datenschutzerklärung</a> zur Kenntnis genommen und stimme zu, dass meine Angaben und Daten zur Beantwortung meiner Anfrage elektronisch erhoben und gespeichert werden."
+					value="akzeptiert"
+					required />
+				<Button class="$w-fit-content">Abschicken</Button>
+			</div>
+
+			{#snippet done()}
+				<hr />
+				<p>Vielen Dank für Ihre Anmeldung.</p>
+			{/snippet}
+		</Form>
+	{/if}
+
+	<div class="$text-center $mt-2">
+		<Link fontello="angle-left" to="../">Zurück zur Übersicht</Link>
 	</div>
-</XioniEventRegistration>
-
-<div class="$text-center $mt-2">
-	<Link fontello="angle-left" to="../">Zurück zur Übersicht</Link>
-</div>
+</Wrapper>
